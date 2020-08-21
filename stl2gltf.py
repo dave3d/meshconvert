507f718c1645c3c1979ed9893f6e8c4dede6ad60
Dave Chen <dchen@mail.nih.gov>
2020-08-21 10:32:09 -0400

    refactored to make more modular

--- stl2gltf.py
+++ stl2gltf.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python
 
+import sys
 import vtk
 
-filename = "teapot.stl"
+def stl2gltf(inname, outname):
 
-reader = vtk.vtkSTLReader()
-reader.SetFileName(filename)
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName(inname)
 
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(reader.GetOutputPort())
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(reader.GetOutputPort())
 
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
 
-# Create a rendering window and renderer
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
+    # Create a rendering window and renderer
+    ren = vtk.vtkRenderer()
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
 
-# Assign actor to the renderer
-ren.AddActor(actor)
+    # Assign actor to the renderer
+    ren.AddActor(actor)
 
-writer = vtk.vtkGLTFExporter()
-writer.SetFileName("teapot.glb")
-writer.InlineDataOn()
-writer.SetRenderWindow(renWin)
-writer.Write()
+    writer = vtk.vtkGLTFExporter()
+    writer.SetFileName(outname)
+    writer.InlineDataOn()
+    writer.SetRenderWindow(renWin)
+    writer.Write()
+
+if __name__ == "__main__":
+
+    inname = "teapot.stl"
+    outname = "teapot.glb"
+
+    if len(sys.argv) > 1:
+        inname = sys.argv[1]
+        if len(sys.argv) > 2:
+            outname = sys.argv[2]
+        else:
+            outname = inname.replace('.stl', '.glb')
+
+    print("Converting", inname, "to", outname)
+    stl2gltf(inname, outname)
