8c5aee922b918ad10c80a73fb705086653647f01
Dave Chen <dchen@mail.nih.gov>
2022-06-09 15:10:47 -0400

    initial checkin

---
+++ glbquery.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import sys
+import trimesh
+import glbutils
+
+inname = sys.argv[1]
+
+print("File:", inname)
+
+scene = trimesh.load(inname)
+
+mesh = glbutils.getSceneMesh(scene)
+
+vsize = mesh.vertices.size
+fsize = mesh.faces.size
+
+print("Vertices: ", int(vsize/3))
+print("Triangles: ", int(fsize/3))
+
